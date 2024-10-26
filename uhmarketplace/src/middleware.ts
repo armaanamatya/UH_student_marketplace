c9be11de59f574e11072644140b64b0476b83b42
Alexander Moreno <alexmore080@gmail.com>
2024-10-26 12:20:48 -0500

    added matchers for next-auth middleware

--- uhmarketplace/src/middleware.ts
+++ uhmarketplace/src/middleware.ts
@@ -1,3 +1,6 @@
 //without a defined matcher, this one line applies next-auth
 // to the entire project
 export { default } from "next-auth/middleware"
+
+// This is where we would restrict certain endpoints for the website to a sign in page
+export const config = { matcher: ["/home"] }
