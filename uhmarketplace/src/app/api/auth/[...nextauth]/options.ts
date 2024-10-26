8c5d9eb9221558dd3e9d2a9275b689e2471daf7b
Alexander Moreno <alexmore080@gmail.com>
2024-10-26 12:24:46 -0500

    restricted only to uh.edu emails through azure authentication

--- uhmarketplace/src/app/api/auth/[...nextauth]/options.ts
+++ uhmarketplace/src/app/api/auth/[...nextauth]/options.ts
@@ -1,6 +1,8 @@
 import type { NextAuthOptions } from "next-auth";
 import AzureAD from "next-auth/providers/azure-ad";
-import CredentialsProvider from "next-auth/providers/credentials";
+
+// I removed support by other means of logging in other than through microsoft authentication. The purpose for this is to truly restrict 
+// access to the website to only uh.edu emails. Authentication will be done through nextauth and azure active directory. 
 
 export const options: NextAuthOptions = {
     providers: [
@@ -8,31 +10,6 @@ export const options: NextAuthOptions = {
             clientId: process.env.AZURE_AD_CLIENT_ID as string,
             clientSecret: process.env.AZURE_AD_CLIENT_SECRET as string,
             tenantId: process.env.AZURE_AD_TENANT_ID as string,
-        }),
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                username: {
-                    label: "Username:",
-                    type: "text",
-                    placeholder: "username-cool"
-                },
-                password: {
-                    label: "Password:",
-                    type: "password"
-                }
-            },
-            async authorize(credentials) {
-                // This is where we would retrieve data from the database
-                // https://next-auth.js.org/configuration/providers/credentials
-                const user = { id: "42", name: "alex", password: "nextauth"}
-
-                if(credentials?.username === user.name && credentials?.password === user.password) {
-                   return user;
-                } else {
-                    return null;
-                }
-            }
         })
     ],
 }
