e855d2502e735323870c5a5666cd578cb6c96ac6
Alexander Moreno <alexmore080@gmail.com>
2024-10-29 09:44:08 -0500

    .env.example file added for guidance/cleaning branch

--- uhmarketplace/src/app/(cb)/client/page.tsx
+++
@@ -1,8 +0,0 @@
-import { getServerSession } from 'next-auth/next';
-import { options } from '../../api/auth/[...nextauth]/options';
-
-export default async function client() {
-    const session = await getServerSession(options);
-    console.log(session?.user);
-    return <h2>Home Page</h2>
-}
