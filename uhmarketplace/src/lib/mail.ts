c9c5f093427cd8aa21296428d41904857ba62715
Alex M <alexmore080@gmail.com>
2024-11-19 19:17:03 -0600

    email

--- uhmarketplace/src/lib/mail.ts
+++ uhmarketplace/src/lib/mail.ts
@@ -11,7 +11,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
     const confirmationLink = `${domain}/verify-email?token=${token}`;
 
     await resend.emails.send({
-        from: "coogbay.xyz",
+        from: "coogbay@coogbay.xyz",
         to: email,
         subject: "Verify your email, Coogbay",
         react: Email({ url:confirmationLink })
